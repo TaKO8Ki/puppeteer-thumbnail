268a0055eaeda405d92deaa76538377b3734b2c6
TaKO8Ki <takoyaki0316@gmail.com>
2019-08-22 13:55:24 +0900

    API側でHTMLを指定するように仕様変更した

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,5 @@
 const puppeteer = require("puppeteer");
 const http = require("http")
-const url = require('url');
 
 //受け取ったhtmlを画像に変換してbase64で返すAPIサーバー
 http.createServer(async function (req, res) {
@@ -18,7 +17,56 @@ http.createServer(async function (req, res) {
         console.error(err);
       });
 
-      let html = JSON.parse(body)['html']
+      //画像に変換するHTML
+      let html = ` 
+      <!doctype html>
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="localhost:stylesheet/application.css">
+      </head>
+      <div id="image">
+        <div class="content">
+          <p class="text">ホゲホゲほげ</p>
+        </div>
+      </div>
+      <style>
+        * {
+          margin: 0;padding: 0;
+        }
+
+        body {
+          font-family: 'M PLUS Rounded 1c', sans-serif;
+        }
+
+        #image {
+          display: table;
+          text-align: center;
+          padding: 0px 30px;
+          width: 540px;
+          height: 314px;
+          background: #16a085;
+        }
+
+        #image div.content {
+          font-size: 25px;
+          font-weight: bold;
+          display: table-cell;
+          vertical-align: middle;
+          word-break: break-all;
+        }
+
+        #image p.text {
+          background: #fff;
+          border-radius: 10px;
+          padding: 110px 0px;
+        }
+      </style> 
+      </html>
+      `;
+
       var buffer = new Buffer(html);
       var string = buffer.toString('base64');
       var img;
@@ -27,9 +75,10 @@ http.createServer(async function (req, res) {
       console.log(html)
       console.log(string)
 
+      res.setHeader('Content-Type', 'application/json');
+
       if (html != undefined) {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
         (async () => {
           const browser = await puppeteer.launch(
             {args: ['--lang=ja,en-US,en']}
@@ -37,7 +86,7 @@ http.createServer(async function (req, res) {
           const page = await browser.newPage();
           const targetElementSelector = '#image'
           await page.goto(`data:text/html;charset=UTF-8;base64,${string}`, { waitUntil: 'networkidle0' });
-          
+
           const clip = await page.evaluate(s => {
             const el = document.querySelector(s)
             const { width, height, top: y, left: x } = el.getBoundingClientRect()
@@ -50,8 +99,10 @@ http.createServer(async function (req, res) {
           res.end(`{"response": "OK", "image": "${img}"}`);
         })();
       } else {
-        res.end(JSON.parse(`{"response": "NG", "image": ""}`));
+        res.statusCode = 400;
+        res.end(`{"response": "NG", "image": ""}`);
       } 
     });
   }
+
 }).listen(3001)
