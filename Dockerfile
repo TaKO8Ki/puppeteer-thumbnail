e1d1f53b8a1c73b3f2d1ed50b14cfab0297a7a4e
TaKO8Ki <takoyaki0316@gmail.com>
2019-09-07 10:53:52 +0900

    dockerやherokuの設定諸々を追加した。

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:10-slim
+
+COPY . /
+
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
+RUN npm i puppeteer \
+    && groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+    && mkdir -p /home/pptruser/Downloads \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /node_modules
+
+USER pptruser
+
+RUN npm install
+
+CMD ["google-chrome-unstable"]
+
+EXPOSE 3000
---
+++ app/thumbnail.js
@@ -0,0 +1,60 @@
+const puppeteer = require("puppeteer");
+const http = require("http")
+require('dotenv').config();
+
+//エンコードされたhtmlを受け取り、それを画像に変換してbase64で返すAPIサーバー
+http.createServer(async function (req, res) {
+  const { headers, method, url } = req;
+  console.log(`${req.method} http://${process.env.DOMAIN}:${process.env.PORT}${req.url}`)
+  
+  if (method == 'POST') {
+    var body = '';
+    req.on('error', (err) => {
+      console.error(err); 
+    }).on('data', (chunk) => {
+      body += chunk
+    }).on('end', () => {
+
+      res.on('error', (err) => {
+        console.error(err);
+      });
+
+      let html = JSON.parse(body)['html']
+      var img;
+
+      res.setHeader('Content-Type', 'application/json');
+
+      if (html != undefined) {
+        res.statusCode = 200;
+        (async () => {
+          const browser = await puppeteer.launch(
+            {
+              args: ['--lang=ja,en-US,en', '--no-sandbox', '--disable-setuid-sandbox'],
+              executablePath: 'google-chrome-unstable',
+            }
+          );
+          const page = await browser.newPage();
+          const targetElementSelector = '#image'
+          await page.goto(`data:text/html;charset=UTF-8;base64,${html}`, { waitUntil: 'networkidle0' });
+
+          const clip = await page.evaluate(s => {
+            const el = document.querySelector(s)
+            const { width, height, top: y, left: x } = el.getBoundingClientRect()
+            return { width, height, x, y }
+          }, targetElementSelector)
+    
+          img = await page.screenshot({clip, encoding: "base64"});
+          await browser.close();
+
+          res.end(`{"response": "OK", "image": "${img}"}`);
+        })();
+      } else {
+        res.statusCode = 400;
+        res.end(`{"response": "NG", "image": ""}`);
+      } 
+    });
+  } else {
+    res.statusCode = 200;
+    res.end(`{"response": "NG", "image": ""}`); 
+  }
+}).listen(process.env.PORT)
